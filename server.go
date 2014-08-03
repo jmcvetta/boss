7ec7a816bcda6d910201c2e39c797690c3d38294
Jason McVetta <jason.mcvetta@gmail.com>
2014-08-02 17:45:01 -0700

    initial stub

---
+++ server.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2014 Jason McVetta.  This is Free Software, released under the
+// terms of the AGPL v3.  See www.gnu.org/licenses/agpl-3.0.html for details.
+// Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
+// Decision Boss is an API to make random decisions from among a set of given
+// choices, and to record similar decisions made on mobile devices.
+package main
+
+import(
+
+)
+
+func main() {
+
+}
